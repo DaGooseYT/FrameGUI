357a53c901528c914997b2a8f56febefa9e92b67
DaGoose <72713589+DaGooseYT@users.noreply.github.com>
2021-08-23 20:57:08 -0400

    Add files via upload

---
+++ FFLoader/Arguments/PresetArgs.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace FFLoader.Arguments
+{
+    internal class PresetArgs
+    {
+        /// <summary>
+        /// The selected output video/cpu preset arguments.
+        /// </summary>
+        /// <param name="preset">The output video/cpu preset in a string format.</param>
+        /// <returns>String representation of the argument.</returns>
+        internal static string VideoPreset(string preset)
+        {
+            return string.Format($" -preset {preset}", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ FFLoader/Handlers/FFMpegErrorHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FFLoader
+{
+    public class FFMpegErrorHandler : EventArgs
+    {
+        /// <summary>
+        /// The error message from FFMpeg console, if exists.
+        /// </summary>
+        public string ErrorMessage { get; private set; }
+
+        /// <summary>
+        /// The FFMpeg error event arguments.
+        /// </summary>
+        /// <param name="message">The FFMpeg error.</param>
+        public FFMpegErrorHandler(string message)
+        {
+            ErrorMessage = message;
+        }
+    }
+}
