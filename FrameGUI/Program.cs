8c16432993ae932408ca5f9f07dd6f1b02468964
DaGoose <72713589+DaGooseYT@users.noreply.github.com>
2021-09-18 18:20:08 -0400

    Added Program.cs (missing initially)

---
+++ FrameGUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrameGUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            SetProcessDPIAware();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrameGUI());
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+    }
+}
